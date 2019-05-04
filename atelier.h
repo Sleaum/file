7458ce91bcda5c22297bd34615710ab0f762675e
Sleaum <sleaum31@gmail.com>
2019-05-04 13:37:56 +0200

    add .gitignore

--- atelier.h
+++ atelier.h
@@ -2,9 +2,9 @@
 #define ATELIER_H
 
 typedef struct Atelier_s Atelier_t;
-typedef struct {
-	int a;
-	int b;
+typedef struct strAtelier_s{
+    int a;
+    int b;
 } strAtelier_t;
 typedef void (*atelierInit_t)(Atelier_t * Atelier);
 typedef int  (*atelierCle1_t)(Atelier_t * Atelier);
@@ -12,9 +12,9 @@ typedef int  (*atelierCle1_t)(Atelier_t * Atelier);
 void atelierInit(Atelier_t * Atelier);
 
 struct Atelier_s {
-  strAtelier_t  str;
-  atelierInit_t init;
-  atelierCle1_t cle1;
+    strAtelier_t  str;
+    atelierInit_t init;
+    atelierCle1_t cle1;
 };
 
 #endif
